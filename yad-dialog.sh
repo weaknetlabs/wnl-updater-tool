2c191fad142d35844af981315754be2befdd729e
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-02-15 13:52:47 -0500

    Create yad-dialog.sh

---
+++ yad-dialog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# GUI display what is happening in the script.
+# To stop, simply kill tail. e.g.: killall -KILL tail 2>/dev/null
+#
+ICON=~/.wt8/images/icons/caffeine-icon.png
+TITLETEXT=$1
+TITLE="Installing Upgrades..."
+SPANFONT="<span font='Ubuntu Condensed 11'>"
+tail -f /etc/issue |yad --progress --pulsate --no-buttons --auto-close \
+  --text="$SPANFONT $1 </span>" --width=350 --center --title="$TITLE" \
+  --window-icon=$ICON --percentage=13 --progress-text="Please Wait..." --image=$ICON
